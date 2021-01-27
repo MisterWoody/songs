a8c42645ee66b886483cc8f59c134be107578a61
Tony Watkins <tony@llifion.net>
2021-01-27 16:05:11 +0000

    Complete the pattern for mapping redux state to props - this will always follow the same pattern

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -8,8 +8,9 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
-    return state;
+    return {
+        songs: state.songs
+    };
 }
 
 export default connect(mapStateToProps)(SongList);
