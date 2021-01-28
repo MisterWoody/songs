c449e2ac673377ec9d56ff7321e50169fbe4d304
Tony Watkins <tony@llifion.net>
2021-01-28 19:56:43 +0000

    Because the name of our objecy property and the function name is the same, we can use es6 shorthand and omit the name of the function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -30,6 +30,4 @@ const mapStateToProps = (state) => {
     };
 }
 
-export default connect(mapStateToProps, {
-    selectSong: selectSong
-})(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
